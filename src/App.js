f0e61bffff69ed6423a577c7fbf7ed24fc6fb795
Jihad0r <Ji.0rabiii@gmail.com>
2024-09-18 14:01:13 +0300

    Do To List

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Crucial } from "./pages/crucial";
+import { Depends } from "./pages/depends";
+import { Irrelevant } from "./pages/irrelevant";
+import { Nav } from "./pages/Navigation";
+import { Sign } from "./pages/signup";
+import { Login } from "./pages/login";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignPage />} />
+        <Route path="/login" element={<LogPage />} />
+        <Route path="/" element={<Nav />}>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/crucial" element={<Crucial />} />
+          <Route path="/depends" element={<Depends />} />
+          <Route path="/irrelevant" element={<Irrelevant />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
+
+const SignPage = () => {
+  let navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Perform form submission logic here
+    navigate("/Home");
+  };
+
+  return <Sign onSubmit={handleSubmit} />;
+};
+
+const LogPage = () => {
+  let navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Perform form submission logic here
+    navigate("/Home");
+  };
+
+  return <Login onSubmit={handleSubmit} />;
+};
 
 export default App;
